cbdec9ce59b88295365e15bd0ee4aee1284699a0
dahee jang <wol04255@gmail.com>
2024-01-18 20:06:15 +0900

    nicco-vanilajs chrome clone coding-4.0

--- C++algorithm/2.DFS,BFS/dfs2.cpp
+++ C++algorithm/2.DFS,BFS/dfs2.cpp
@@ -3,14 +3,16 @@ using namespace std;
 //2차원 배열 dfs
 //배열 크기는 넉넉하게 최대값으로
 int n, m, ret;
-vector<vector<int>> a;
-vector<bool> vst;
+bool a[104][104], vst[104][104];
+int dy[4] = { -1, 0, 1, 0}, dx[4] = { 0, 1, 0, -1};
+void dfs(int y, int x){
+  vst[y][x] = 1;
 
-void dfs(int u){
-  vst[u] = 1;
-
-  for(int v : a[u]){
-    if(vst[v]==0) dfs(v);
+  for(int i = 0; i < 4; i++){
+    int ny = y + dy[i];
+    int nx = x + dx[i];
+    if(ny >= n || ny < 0 || nx >= m || nx < 0) continue;
+    if(!vst[ny][nx] && a[ny][nx]) dfs(ny, nx);
   }
   return;
 }
@@ -20,14 +22,17 @@ int main(){
 
   for(int i = 0; i < n; i++){
     for(int j = 0; j < m; j++){
-      bool tmp;
-      cin >> tmp;
-      if(tmp) a[i].push_back(j);
+      cin >> a[i][j];
     }
   }
 
   for(int i = 0; i < n; i++){
-    if(vst[i] == 0) dfs(i); ret++;
+    for(int j = 0; j < m; j++){
+      if(a[i][j] == 1 && vst[i][j] == 0) {
+        dfs(i, j); 
+        ret++;}
+    } 
+    
   }
 
   cout << ret <<"\n";
